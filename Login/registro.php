514af163440eef8066c3ad5dca1e1d5c99eceb5b
Espila26 <epicado2010@gmail.com>
2018-03-13 21:35:22 -0600

    Form Registro
    Se corrigio el foprm de registro de usuarios.

--- Login/registro.php
+++ Login/registro.php
@@ -73,30 +73,24 @@ button:hover {
 </style>
 <body>
 
-<form action="/action_page.php" style="border:1px solid #ccc">
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" style="border:1px solid #ccc" method="post">
   <div class="container">
-    <h1>Sign Up</h1>
-    <p>Please fill in this form to create an account.</p>
+    <h1>Registro de Nuevos Usuarios</h1>
+    <p>Complete la información para registrar nuevos usuarios.</p>
     <hr>
 
-    <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" required>
+    <label for="email"><b>Nombre de Usuario</b></label>
+    <input type="text" placeholder="Ingresa Usuario" name="username" required>
 
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
+    <label for="psw"><b>Contraseña</b></label>
+    <input type="password" placeholder="Ingresa Contraseña" name="psw" required>
 
-    <label for="psw-repeat"><b>Repeat Password</b></label>
-    <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
-    
-    <label>
-      <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
-    </label>
-    
-    <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+    <label for="psw-repeat"><b>Repita Contraseña</b></label>
+    <input type="password" placeholder="Ingresa de nuevo la Contraseña" name="psw-repeat" required>
 
     <div class="clearfix">
-      <button type="button" class="cancelbtn">Cancel</button>
-      <button type="submit" class="signupbtn">Sign Up</button>
+      <button type="button" class="cancelbtn">Cancelar</button>
+      <button type="submit" class="signupbtn">Registrar</button>
     </div>
   </div>
 </form>
