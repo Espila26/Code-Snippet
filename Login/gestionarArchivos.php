90c9a552b127da8a42f793d5cd8e87dc64aef869
JordyRG21 <jordyromgu@hotmail.com>
2018-03-19 20:10:14 -0600

    se arreglo el mostrar agregar y editar

--- Login/gestionarArchivos.php
+++ Login/gestionarArchivos.php
@@ -6,22 +6,24 @@
 	<link href="estilos/gestionarArchivos.css" type="text/css" rel="stylesheet"/>
 	<?php
 	session_start();
-	
+		
 	if( !isset( $_SESSION['userName'] ))
 		header("Location: Login.php");
 	
-	$metaArray = readMetaFile( 'metaData.txt' );
 	$metaDataFile = initializeFile( 'metaData.txt' );
+	$metaIndexfile = initializeFile( "metaIndex.txt" );
+	$metaArray = readMetaFile( 'metaData.txt' );
+	$metaIndexArray = readMetaFile( 'metaIndex.txt' );
 	$usersfile = initializeFile( "usuarios.txt" );
 	$usersArray = unserializeUsersData( $usersfile );
 	if( isset( $_POST[ 'submit' ] ) ){
-		uploadFile( $metaDataFile, $metaArray );
+		uploadFile( $metaDataFile, $metaArray, $metaIndexfile, $metaIndexArray );
 	}
-	if( isset( $_POST[ 'edit' ] ) || isset( $_POST[ 'delete' ] )){
+	if( isset( $_POST[ 'edit' ] ) || isset( $_POST[ 'delete' ] ) || isset( $_POST[ 'show' ] )){
 		echo"yes";
 		if( isset( $_POST[ 'metaDataCheckBox' ] ) ){
 			$valuesChecked = getCheckBoxValues( $_POST[ 'metaDataCheckBox' ]);
-			if( isset( $_POST[ 'edit' ] )){
+			if( isset( $_POST[ 'edit' ] ) || isset( $_POST[ 'show' ] )){
 				if(count($valuesChecked)>1){
 
 					header("Location: gestionarArchivos.php");
@@ -44,18 +46,32 @@
 	if( isset( $_POST[ 'submitEdit' ] )){
 		$sharedWithString = '';
 		if( isset( $_POST[ 'sharedWith' ] )){
-			var_dump( $_POST[ 'sharedWith' ] );
-			foreach( $_POST[ 'sharedWith' ] as $selectedOption )
+		foreach( $_POST[ 'sharedWith' ] as $selectedOption )
 				$sharedWithString = $sharedWithString . ',' . $selectedOption;
 		}
 		var_dump( $sharedWithString );
+		$description ="";$metaName =""; $author="";$date="";$clasification = "";
+			
+		if( isset( $_POST[ 'addName' ] ))
+			$metaName = $_POST[ 'addName' ];
+		if( isset( $_POST[ 'addDescription' ] ))
+			$description = $_POST[ 'addDescription' ];
+		if( isset( $_POST[ 'addAuthor' ] ))
+			$author = $_POST[ 'addAuthor' ];
+		if( isset( $_POST[ 'addDate' ] ))
+			$date = $_POST[ 'addDate' ];
+		if( isset( $_POST[ 'addClasification' ] ))
+			$clasification = $_POST[ 'addClasification' ];
 		$index = $_POST[ 'metaIndex' ];
 		$valueToEdit = $metaArray[ $index ];
-		$valueToEdit[ 'metaName' ] = $_POST[ 'metaName' ];
-		$valueToEdit[ 'description' ] = $_POST[ 'metaDescription' ];
-		$valueToEdit[ 'sharedWith' ] = $sharedWithString;
+		$valueToEdit[ 'metaName' ] = $metaName;
+		$valueToEdit[ 'description' ] = $description;
+		$valueToEdit[ 'author' ] = $author;
+		$valueToEdit[ 'date' ] = $date;
+		$valueToEdit[ 'clasification' ] = $clasification;
+		$valueToEdit[ 'sharedWith' ] = '';
 		$metaArray[ $index ] = $valueToEdit;
-		saveFileSerialized( $metaDataFile, $metaArray );
+		saveFileSerialized( $metaDataFile, $metaArray, true );
 	}
 		
 	if( isset( $_POST[ 'logout' ] )){
@@ -63,15 +79,29 @@
 		header("Location: Login.php");
 	}
 	
-	function uploadFile( $metaDataFile, $metaArray ){
+	function uploadFile( $metaDataFile, $metaArray, $metaIndexfile, $metaIndexArray ){
 		if ( $_FILES[ 'archivo' ][ "error" ] > 0 ){
 			echo "Error: " . $_FILES[ 'archivo' ][ 'error' ] . "<br>";
 		}else{
+			$description ="";$metaName =""; $author="";$date="";$clasification = "";
+			
+			if( isset( $_POST[ 'addName' ] ))
+				$metaName = $_POST[ 'addName' ];
+			if( isset( $_POST[ 'addDescription' ] ))
+				$description = $_POST[ 'addDescription' ];
+			if( isset( $_POST[ 'addAuthor' ] ))
+				$author = $_POST[ 'addAuthor' ];
+			if( isset( $_POST[ 'addDate' ] ))
+				$date = $_POST[ 'addDate' ];
+			if( isset( $_POST[ 'addClasification' ] ))
+				$clasification = $_POST[ 'addClasification' ];
+			
 			$name = $_FILES[ 'archivo' ][ 'name' ]."";
 			$size = round( $_FILES[ 'archivo' ][ 'size' ] / 1024 / 1024, 3);
 			$path =  $_SESSION[ 'userName' ];
-			buildMetaData( $metaDataFile, $metaArray, "MetaData para: " . $name, $name, "uploadedFile",
-                    		$path, $_SESSION[ 'userName' ], " ", $size, false);
+			buildMetaData( $metaDataFile, $metaArray, $metaName, $name, $description,
+                    		$path, $_SESSION[ 'userName' ], " ", $size, false, $author, 
+							$date, $clasification,$metaIndexfile, $metaIndexArray);
 
 			//chdir( 'C:\\ProjectDirectories\\' );// Change the directory where we are to the one we want
 			move_uploaded_file( $_FILES[ 'archivo' ][ 'tmp_name' ],
@@ -124,15 +154,23 @@
 		return $array;
 	}
 	
-	function buildMetaData( $file, $metaDataArray, $metaName , $name, $description, $path, $owner, $sharedWith, $size, $deleted ){
+	function buildMetaData( $file, $metaDataArray, $metaName , $name, $description, $path, $owner, 
+							$sharedWith, $size, $deleted, $author, $date, $clasification, $metaIndexfile, $metaIndexArray ){
 		$count = count( $metaDataArray );
-		$metaData = array( 'id' => $count, 'metaName' => $metaName, 'realName' => $name, 'description' => $description,
-                       	   'path' => $path, 'owner' => $owner, 'sharedWith' => $sharedWith, 'size' => $size, 'deleted' => $deleted);
+		$metaData = array( 'metaName' => $metaName, 'description' => $description,
+                       	   'path' => $path, 'owner' => $owner, 'sharedWith' => $sharedWith, 
+						   'size' => $size, 'deleted' => $deleted, 'author' => $author, 
+						   'date' => $date, 'clasification' => $clasification);
 		array_push( $metaDataArray, $metaData );
-		saveFileSerialized( $file, $metaDataArray );
+		
+		$metaIndex = array( 'id' => $count, 'realName' => $name );
+		array_push( $metaIndexArray, $metaIndex );
+		
+		saveFileSerialized( $file, $metaDataArray, false );
+		saveFileSerialized( $metaIndexfile, $metaIndexArray, true );
 	}
 	
-	function saveFileSerialized( $file, $array ){
+	function saveFileSerialized( $file, $array, $reload ){
 		var_dump($array);
 		$newArray = [];
 		foreach( $array as $data){
@@ -140,7 +178,8 @@
 		}                                              //array que guardara toda la metadata y luego se serializa el
 		$string = serialize( $newArray );              // array que contiene todas las metadatas
 		fwrite( $file, $string );
-		header("Location: gestionarArchivos.php");
+		if( $reload )
+			header("Location: gestionarArchivos.php");
 	}
 	
 	function readMetaFile( $file ){
@@ -179,17 +218,48 @@
 	</header>
     <body>
 		<h1>Administrador de Archivos PHP</h1>
-		<div class="uploadFile">
-			<h2>Archivo a subir:</h2>
-			<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
-				<input class="custom-file-input" type="file" name="archivo" id="archivo"></input></br></br>
-				<input class='button'  type="submit" name="submit" value="Subir archivo"></input>
+		<div class="add">
+		<?php
+		$title = "Archivo a subir:";
+		if( isset( $_POST[ 'edit' ] )|| isset( $_POST[ 'show' ] )){
+			$index = $valuesChecked[0];
+			$current = $metaArray[ $index ];
+			$title = "Editar Archivo:";
+		}
+		if( !isset( $current ) ){
+			$current = array( 'metaName' => '', 'description' => '', 'path' => '', 'owner' => '', 'sharedWith' => '', 
+							  'size' => '', 'deleted' => '', 'author' => '', 'date' => '', 'clasification' => '');
+		}
+			echo"<h2>".$title."</h2>
+			<form action='".$_SERVER['PHP_SELF']."' method='post' enctype='multipart/form-data'>
+				<label for='addName'>Nombre</label>
+				<input  class='textAdd' type='text' name='addName' placeholder='Nombre del archivo..' value='".$current[ 'metaName' ]."'>
+				<label for='addAuthor'>Autor</label>
+				<input  class='textAdd' type='text' name='addAuthor' placeholder='Nombre del autor..' value='".$current[ 'author' ]."'>
+				<label for='addDate'>Fecha</label>
+				<input  class='textAdd' type='date' name='addDate' value='".$current[ 'date' ]."'>
+				<label for='addClasification'>Clasificacion</label>
+				<input  class='textAdd' type='text' name='addClasification' placeholder='Clasificacion del archivo..' value='".$current[ 'clasification' ]."'>
+				<label for='sharedWith'>Compartido Con:</label>
+				<select name='sharedWith[]' size='1' multiple='multiple' tabindex='1' >";
+				foreach( $usersArray as $array ){
+					echo"<option value='".$array[ 'username' ]."'>".$array[ 'username' ]."</option>";}
+				echo"</select>
+				<label for='addDescription'>Descripcion</label>
+				<textarea class='textAdd' type='text'; name='addDescription' placeholder='Descripcion del archivo..' value='".$current[ 'description' ]."'></textarea>";
+				if( !isset( $_POST[ 'edit' ] ) && !isset( $_POST[ 'show' ] ) ){
+				echo"<input class='custom-file-input' type='file' name='archivo' id='archivo'></input></br></br>
+				<input class='button'  type='submit' name='submit' value='Subir archivo'></input>";
+				}else if( isset( $_POST[ 'edit' ] ) ){
+					echo"<input class='inputEdit' type='submit' name='submitEdit' value='Submit'>
+					<input  class='textEdit' type='text' name='metaIndex'  value = '" .$index. "' >";
+				}
+				echo"
 			</form>
 		</div>
 		
-		<div class="showFiles">
-		<?php
-		if( isset( $metaArray ) && !isset( $_POST['edit'] )){
+		<div class='showFiles'>";
+		if( isset( $metaArray ) ){
 		$contFiles = 0;
 		$contFolders = 0;
 		$totalSize = 0;
@@ -201,15 +271,17 @@
 				<th>Tamanno</th>
 				<th>Accion</th>
 			</tr>";
-			foreach( $metaArray as $array ){
-				if( $array['owner'] == $_SESSION[ 'userName' ] && $array[ 'deleted' ] == false ){
+			foreach( $metaIndexArray as $array ){
+				$index = $array[ 'id' ];
+				$current = $metaArray[ $index ];
+				if( $current['owner'] == $_SESSION[ 'userName' ] && $current[ 'deleted' ] == false ){
 					echo"<tr>
-							<td> <a href='../".$array[ 'path' ]."/".$array[ 'realName' ]."' download> ".$array[ 'metaName' ]." </a> </td>
-							<td> ".$array['size']." MB  </td>
+							<td> <a href='../".$current[ 'path' ]."/".$array[ 'realName' ]."' download> ".$current[ 'metaName' ]." </a> </td>
+							<td> ".$current['size']." MB  </td>
 							<td> <input name=metaDataCheckBox[] type=checkbox value= ".$array['id']." > </td>
 						 </tr>";
 						 $contFiles++;
-						 $totalSize = $totalSize + $array[ 'size' ];
+						 $totalSize = $totalSize + $current[ 'size' ];
 				}
 			}
 			echo" <tr><td></td><td>".$contFiles." Archivos(" .$totalSize. " MB) </td>
@@ -218,27 +290,6 @@
 			<input class='button' type='submit' name='delete' value='Eliminar'> </input> </td></tr>
 		</table>
 		</form>";
-		}else if( isset( $_POST['edit'] )){
-			$index = $valuesChecked[0];
-			$value = $metaArray[ $index ];
-		echo"<h2>Editar Archivo</h2>
-		<div class='edit'>
-		  <form action= ".$_SERVER['PHP_SELF']." method='post'>
-			<label for='metaName'>Nombre</label>
-			<input  class='textEdit' type='text' name='metaName' placeholder='Nombre del archivo..' value = '" .$value['metaName']. "' >
-			<input  class='textEdit' type='text' name='metaIndex'  value = '" .$index. "' >
-
-			<label for='sharedWith'>Compartido Con:</label>
-			<select name='sharedWith[]' size='3' multiple='multiple' tabindex='1'>";
-			foreach( $usersArray as $array ){
-				echo"<option value='".$array[ 'username' ]."'>".$array[ 'username' ]."</option>
-			";}echo"
-			</select>
-			<label for='description'>Descripcion</label>
-			<textarea class='textEdit' type='text'; name='metaDescription' placeholder='Descripcion del archivo..'>".$value['description']. "</textarea>
-			<input class='inputEdit' type='submit' name='submitEdit' value='Submit'>
-		  </form>
-		</div>";
 		}?>
 		</div>		
     </body>
